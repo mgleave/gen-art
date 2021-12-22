b30b23bc2c77c8fd197c58566bb444561befd314
mgleave <m.gleave@nithio.com>
2021-12-22 12:10:09 -0700

    Create initial generativeart package tutorial/exploration

---
+++ generativeart_pkg_test.R
@@ -0,0 +1,33 @@
+# Original testing file with generativeart package
+
+devtools::install_github("cutterkom/generativeart")
+
+library(generativeart)
+
+# Setup per documentation at cutterkom/generativeart
+
+# set the paths
+IMG_DIR <- "img/"
+IMG_SUBDIR <- "everything/"
+IMG_SUBDIR2 <- "handpicked/"
+IMG_PATH <- paste0(IMG_DIR, IMG_SUBDIR)
+
+LOGFILE_DIR <- "logfile/"
+LOGFILE <- "logfile.csv"
+LOGFILE_PATH <- paste0(LOGFILE_DIR, LOGFILE)
+
+# create the directory structure
+generativeart::setup_directories(IMG_DIR, IMG_SUBDIR, IMG_SUBDIR2, LOGFILE_DIR)
+
+
+
+## Example
+
+# include a specific formula, for example:
+my_formula <- list(
+  x = quote(runif(1, -1, 1) * x_i^2 - sin(y_i^5)),
+  y = quote(runif(1, -1, 1) * y_i^3 - cos(x_i^2))
+)
+
+# call the main function to create five images with a polar coordinate system
+generativeart::generate_img(formula = my_formula, nr_of_img = 3, polar = TRUE, filetype = "png", color = "black", background_color = "white")
